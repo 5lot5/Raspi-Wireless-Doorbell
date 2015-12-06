a367ea0648ee68055ecf9332242d66227e18e5f1
5lot5 <5lot5@telenet.be>
2015-12-07 00:37:59 +0100

    Raspi wireless doorbell
    This python script reads the status of a GPIO on the raspberry-pi and
    sends an email upon trigger.

---
+++ README.md
@@ -0,0 +1,3 @@
+This python script reads the status of a GPIO on the raspberry-pi and sends an email upon trigger.
+
+Free to use and change.
---
+++ doorbell_monitor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+'''
+   This script checks the status of my wireless doorbell and sends
+   an email to my cellphone when someone is at the door
+
+   written by 5lot5 - 5lot5 at telenet dot be
+
+'''
+
+import RPi.GPIO as GPIO
+import os, time
+
+doorbell = 40
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(doorbell, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+status = GPIO.input(doorbell)
+file2mail = open("/tmp/file2mail.tmp", "w")
+
+while True:
+      status = GPIO.input(doorbell)
+      if status == 0:
+           logtime = time.strftime("%d-%m-%Y %H:%M")
+           os.system("echo "+logtime+" >> /var/log/doorbell.log")
+           file2mail.write("From: Doorbell@Home\nTo:YourEmail@Yourmailserver.com\nSubject:You got a visitor at the door!\n\n"+logtime+"\n")
+           file2mail.close()
+           os.system("ssmtp YourEmail@Yourmailserver.com < /tmp/file2mail.tmp")
+           print 'done.'
+           time.sleep(7)
+      else:
+           time.sleep(1)
+           pass
